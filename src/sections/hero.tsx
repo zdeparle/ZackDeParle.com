89bbdf5e1516580c02ff3533f34c3e1b1898cfb2
Zachary DeParle <zachary.deparle@duke.edu>
2023-11-09 13:42:46 -0500

    innovator

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -29,7 +29,7 @@ export default function hero() {
         <MotionDiv delayOffset={1}>Engineer</MotionDiv>
       </h1>
       <h1>
-        <MotionDiv delayOffset={1.2}>Artist</MotionDiv>
+        <MotionDiv delayOffset={1.2}>Innovator</MotionDiv>
       </h1>
       <div className="my-12 flex w-full flex-col gap-2 text-center lg:w-[50%]">
         <MotionDiv delayOffset={1.2}>
