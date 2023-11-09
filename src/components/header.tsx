37f8245c1424a1e0d8458e89a38075658ba617c3
Zachary DeParle <zachary.deparle@duke.edu>
2023-11-09 16:36:37 -0500

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from 'next/image'; // Importing Image component
 
 export default function Header() {
-  const links = ["about", "skills", "projects", "experiences", "contact"];
+  const links = ["about", "skills", "projects", "experience", "contact"];
 
   return (
     <nav className="justify-center py-2 flex w-full select-none pt-6 font-light md:px-28 md:pb-2">
@@ -24,7 +24,7 @@ export default function Header() {
         <div className="nav-links flex gap-x-8 text-xs md:text-base">
           {links.map((link) => (
             <Link key={link} href={`/${link === "experiences" ? "experiences" : `#${link}`}`} legacyBehavior>
-              <a className="cursor-pointer hover:underline" onClick={(e) => {
+              <a className="cursor-pointer" onClick={(e) => {
                 if (link !== "projects" && link !== "experiences") {
                   e.preventDefault();
                   document.getElementById(link)?.scrollIntoView({ behavior: "smooth" });
@@ -38,7 +38,7 @@ export default function Header() {
           <a
             href="/Zachary_DeParle_Resume_2023.pdf" // Update this path to the actual path of your resume
             download="Zachary_DeParle_Resume_2023.pdf"
-            className="cursor-pointer hover:underline text-white bg-blue-600 px-4 py-2 rounded-md shadow-lg hover:bg-blue-700"
+            className="cursor-pointer text-white bg-blue-600 px-4 py-2 rounded-md shadow-lg hover:bg-blue-700"
           >
             Resume
           </a>
