04d40d8b19fbd8116c6dc51f9cca0a10a0942c8a
Zachary DeParle <zachary.deparle@duke.edu>
2023-11-09 23:28:52 -0500

    prodigyheaderupdates

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,16 @@
-"use client";
-import Link from "next/link";
-import Image from 'next/image'; // Importing Image component
+import Link from 'next/link';
+import Image from 'next/image';
+
+// Add your LinkedIn logo image path here
+import linkedinLogo from './linkedin-logo.png'; 
 
 export default function Header() {
   const links = ["about", "skills", "projects", "experience", "contact"];
 
   return (
-    <nav className="justify-center py-2 flex w-full select-none pt-6 font-light md:px-28 md:pb-2">
-      <div className="container flex flex-col items-center justify-between md:flex-row">
+    <nav className="flex w-full justify-center py-2 select-none pt-6 font-light md:px-28 md:pb-2">
+      <div className="container flex flex-col md:flex-row items-center justify-between">
+        {/* Logo */}
         <div className="text-5xl drop-shadow-2xl">
           <Link href="/" legacyBehavior>
             <a>
@@ -21,26 +24,32 @@ export default function Header() {
             </a>
           </Link>
         </div>
+        {/* Navigation Links */}
         <div className="nav-links flex gap-x-8 text-xs md:text-base">
           {links.map((link) => (
-            <Link key={link} href={`/${link === "experiences" ? "experiences" : `${link}`}`} legacyBehavior>
-              <a className="cursor-pointer" onClick={(e) => {
-                if (link !== "projects" && link !== "experiences") {
-                  e.preventDefault();
-                  document.getElementById(link)?.scrollIntoView({ behavior: "smooth" });
-                }
-              }}>
+            <Link key={link} href={`/#${link}`} legacyBehavior>
+              <a className="cursor-pointer hover:text-blue-500">
                 {link.charAt(0).toUpperCase() + link.slice(1)}
               </a>
             </Link>
           ))}
           {/* Resume Download Button */}
           <a
-            href="/Zachary_DeParle_Resume_2023.pdf" // Update this path to the actual path of your resume
+            href="/Zachary_DeParle_Resume_2023.pdf"
             download="Zachary_DeParle_Resume_2023.pdf"
-            className="cursor-pointer text-white bg-blue-600 px-4 py-2 rounded-md shadow-lg hover:bg-blue-700"
+            className="inline-block bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md shadow-lg"
           >
             Resume
+          </a>
+          {/* LinkedIn Button */}
+          <a href="https://www.linkedin.com/in/zachary-deparle" target="_blank" rel="noopener noreferrer"
+             className="block relative h-10 w-10"> {/* Adjust the height and width as needed */}
+            <Image
+              src={linkedinLogo}
+              alt="LinkedIn"
+              layout="fill"
+              objectFit="contain"
+            />
           </a>
         </div>
       </div>
