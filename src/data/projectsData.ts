19228a7fe129ccc4f39d440dd84eceda65843212
Zachary DeParle <zachary.deparle@duke.edu>
2023-11-16 00:37:35 -0500

    fixed project modality

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,28 +1,34 @@
 // src/data/projectsData.ts
 
-interface Project {
+export interface Project {
     id: number;
     title: string;
-    description: string;
-    thumbnail: string;
-    // Add other project properties here
+    thumbnail: string; // URL or path to the thumbnail image
+    images: string[]; // Array of URLs or paths to additional images
+    description: string; // Short description of the project
+    detailedDescription: string; // Detailed description, can include HTML tags if needed
+    cadLink?: string; // Optional: URL to the CAD viewer for this project
+    // ... you can add more properties as needed
   }
   
   export const projectsData: Project[] = [
     {
       id: 1,
-      title: "Project One",
-      description: "Description for Project One",
-      thumbnail: '/path-to-thumbnail-1.jpg',
-      // other details...
+      title: 'Project One with Autodesk CAD',
+      thumbnail: '/path/to/thumbnail1.jpg',
+      images: [
+        '/path/to/image1.jpg',
+        '/path/to/image2.jpg',
+        // ... more images
+      ],
+      description: 'Short description of Project One',
+      detailedDescription: `
+        <p>Detailed information about Project One.</p>
+        <p>More details...</p>
+        <!-- You can use HTML tags for formatting if you plan to render this as dangerouslySetInnerHTML in React. -->
+      `,
+      cadLink: 'https://autode.sk/3G6fzR2', // Your Autodesk CAD viewer link
     },
-    {
-      id: 2,
-      title: "Project Two",
-      description: "Description for Project Two",
-      thumbnail: '/path-to-thumbnail-2.jpg',
-      // other details...
-    },
-    // ...more projects
+    // ... other project objects
   ];
   
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,33 +1,22 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Head from 'next/head';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import styles from '../styles/projects.module.css';
-
-// Define your project data structure and data here
-const projectsData = [
-  {
-    id: 1,
-    title: "Project One",
-    description: "Description for Project One",
-    thumbnail: '/path-to-thumbnail-1.jpg',
-    // Add other project details like images and descriptions
-  },
-  {
-    id: 2,
-    title: "Project Two",
-    description: "Description for Project Two",
-    thumbnail: '/path-to-thumbnail-2.jpg',
-    // Add other project details like images and descriptions
-  },
-  // ...more projects
-];
+import { Project, projectsData } from '../data/projectsData'; // Import the Project interface and projects data
 
 const Projects = () => {
-  const [activeProjectId, setActiveProjectId] = useState<number | null>(null);
+  const [activeProject, setActiveProject] = useState<Project | null>(null);
 
-  const toggleProject = (id: number) => {
-    setActiveProjectId(activeProjectId === id ? null : id);
+  const toggleProject = (project: Project) => {
+    setActiveProject(activeProject === project ? null : project);
+  };
+
+  // This function will be called when the backdrop is clicked
+  const handleBackdropClick = (event: React.MouseEvent) => {
+    // Prevent clicks from the modal content from reaching the backdrop
+    event.stopPropagation();
+    setActiveProject(null); // Close the modal
   };
 
   // Animation variants for Framer Motion
@@ -42,7 +31,7 @@ const Projects = () => {
         <title>Projects | Zack DeParle's Portfolio</title>
       </Head>
       <div className={styles.gridContainer}>
-        {projectsData.map((project) => (
+        {projectsData.map((project: Project) => (
           <motion.div
             key={project.id}
             className={styles.gridItem}
@@ -50,21 +39,62 @@ const Projects = () => {
             initial="hidden"
             animate="visible"
             whileHover={{ scale: 1.05 }}
-            onClick={() => toggleProject(project.id)}
+            onClick={() => toggleProject(project)}
           >
             <Image
               src={project.thumbnail}
               alt={`${project.title} Thumbnail`}
               width={300} // Set desired width
               height={200} // Set desired height
-              layout="responsive" // This will make the image responsive
-              className={styles.thumbnailImage}
+              layout="responsive"
             />
             <h2>{project.title}</h2>
-            {activeProjectId === project.id && <p className={styles.projectDescription}>{project.description}</p>}
           </motion.div>
         ))}
       </div>
+      <AnimatePresence>
+        {activeProject && (
+          <motion.div
+            className={styles.fullScreenModal}
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            onClick={handleBackdropClick} // This handles clicking outside the modal content
+          >
+            <div
+              className={styles.modalContent}
+              onClick={(e) => e.stopPropagation()} // Prevent clicks inside the modal content from closing it
+            >
+              <button onClick={() => setActiveProject(null)}>Close</button>
+              <h2>{activeProject.title}</h2>
+              <p>{activeProject.description}</p>
+              {/* Render all images */}
+              {activeProject.images.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image}
+                  alt={`${activeProject.title} Image ${index + 1}`}
+                  width={800} // Actual width of the image
+                  height={600} // Actual height of the image, maintaining the aspect ratio
+                  layout="responsive"
+                />
+              ))}
+              {/* CAD Viewer - make sure this iframe only renders when there is a CAD link available */}
+              {activeProject.cadLink && (
+                <iframe
+                  src={activeProject.cadLink}
+                  frameBorder="0"
+                  allowFullScreen
+                  className={styles.cadViewer} // Apply styling through this class
+                ></iframe>
+              )}
+              {/* Detailed Description */}
+              <div dangerouslySetInnerHTML={{ __html: activeProject.detailedDescription }} />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
