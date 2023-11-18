f462bae7f140ba0f184f32919fbaee61776879a0
Zachary DeParle <zachary.deparle@duke.edu>
2023-11-17 21:39:35 -0500

    fixed header formatting and linkedin button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link';
 import Image from 'next/image';
-
-// Add your LinkedIn logo image path here
-import linkedinLogo from './linkedin-logo.png'; 
+import linkedinLogo from './linkedin-logo.png'; // Ensure this path is correct
 
 export default function Header() {
   const links = [
-    
     { name: 'About', path: '/#about' },
     { name: 'Skills', path: '/#skills' },
     { name: 'Projects', path: '/projects' },
@@ -18,37 +15,37 @@ export default function Header() {
     <nav className="flex w-full justify-center py-2 select-none pt-6 font-light md:px-28 md:pb-2">
       <div className="container flex flex-col md:flex-row items-center justify-between">
         {/* Logo */}
-        <div className="text-5xl drop-shadow-2xl">
-          <Link href="/" legacyBehavior>
-            <a>
-              <Image
-                src="/RectangleZLogo.png"
-                alt="Zack DeParle"
-                width={100}
-                height={45}
-                objectFit="contain"
-              />
-            </a>
-          </Link>
-        </div>
+        <Link href="/" passHref>
+          <div className="text-5xl drop-shadow-2xl cursor-pointer">
+            <Image
+              src="/RectangleZLogo.png"
+              alt="Zack DeParle"
+              width={100}
+              height={45}
+              objectFit="contain"
+            />
+          </div>
+        </Link>
         {/* Navigation Links */}
-        <div className="nav-links flex gap-x-8 text-xs md:text-base">
+        <div className="nav-links flex flex-grow items-center justify-center gap-x-8 text-xs md:text-base">
           {links.map((link) => (
-            <Link key={link.name} href={link.path} legacyBehavior>
-              <a className="cursor-pointer hover:text-blue-500">
-                {link.name.charAt(0).toUpperCase() + link.name.slice(1)}
-              </a>
+            <Link key={link.name} href={link.path} passHref>
+              <span className="cursor-pointer hover:text-blue-500 transition-colors duration-200">
+                {link.name}
+              </span>
             </Link>
           ))}
-          
-          {/* LinkedIn Button */}
-          <a href="https://www.linkedin.com/in/zachary-deparle" target="_blank" rel="noopener noreferrer"
-             className="flex items-center justify-center h-5 w-5"> {/* Use flexbox to center the image */}
+        </div>
+        {/* LinkedIn Button */}
+        <div className="flex items-center justify-end">
+          <a href="https://www.linkedin.com/in/zachary-deparle" target="_blank" rel="noopener noreferrer" className="ml-4">
             <Image
               src={linkedinLogo}
               alt="LinkedIn"
-              width={40} // Explicitly set width
-              height={40} // Explicitly set height
+              width={40}
+              height={40}
+              objectFit="contain"
+              className="cursor-pointer"
             />
           </a>
         </div>
