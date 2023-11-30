2826204c2a1d985359f3a4abfc6d0ea656504a40
Zachary DeParle <zachary.deparle@duke.edu>
2023-11-30 01:22:49 -0500

    added engineering drawings carousel to vise project page

--- src/pages/projects/vise.tsx
+++ src/pages/projects/vise.tsx
@@ -1,8 +1,21 @@
 import Head from 'next/head';
 import { motion } from 'framer-motion';
-import styles from '/src/styles/vise.module.css'; // Ensure the path is correct
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import styles from '/src/styles/vise.module.css';
 
 const Vise = () => {
+  const drawingImages = [
+    '/vise/drawing1.jpg',
+    '/vise/drawing2.jpg',
+    '/vise/drawing3.jpg',
+    '/vise/drawing4.jpg',
+    '/vise/drawing5.jpg',
+    '/vise/drawing6.jpg',
+    // ... other image paths ...
+  ];
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -14,6 +27,7 @@ const Vise = () => {
       </Head>
       <main className={styles.mainContent}>
         <h1 className={styles.title}>Vise Stop Project</h1>
+        {/* ... Description paragraphs ... */}
         <p className={styles.description}>
           I completed a project where I machined and assembled a vise stop, a useful tool for machining. This involved studying detailed engineering drawings, understanding the functions of vise stops, and preparing machining plans for the parts "Vise Jaw - Left" and "Vise Jaw - Right." I paid particular attention to engineered fits, reference dimensions, and tolerances, which required precise machining operations.
         </p>
@@ -24,14 +38,28 @@ const Vise = () => {
           Throughout the project, I focused on safety and efficiency, asking for guidance when needed. My final step involved marking my assembled vise stop with my last name before submission. This project not only enhanced my machining skills but also taught me the importance of meticulous planning and execution in mechanical design.
         </p>
         <iframe 
-        src="https://myhub.autodesk360.com/ue2a9dcff/shares/public/SH512d4QTec90decfa6e76f3d59aac9339d2?mode=embed" 
-        width="640" 
-        height="480" 
-        allowFullScreen 
-        frameBorder="0"
-        className={styles.cadIframe}
-  ></iframe>
+          src="https://myhub.autodesk360.com/ue2a9dcff/shares/public/SH512d4QTec90decfa6e76f3d59aac9339d2?mode=embed" 
+          width="640" 
+          height="480" 
+          allowFullScreen 
+          frameBorder="0"
+          className={styles.cadIframe}
+        ></iframe>
 
+        <h2 className={styles.sectionTitle}>Engineering Drawings</h2>
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={1}
+          navigation
+          modules = {[Navigation]}
+          className={styles.swiperContainer}
+        >
+          {drawingImages.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img src={image} alt={`Drawing ${index + 1}`} className={styles.swiperImage} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </main>
     </motion.div>
   );
